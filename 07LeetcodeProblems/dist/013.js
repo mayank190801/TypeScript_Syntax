6d3cbc6aec4ad0afb7443bfbe116a248aceb1013
Mayank Singh <mayank190801@gmail.com>
2023-01-29 22:44:44 +0530

    2520. Count the Digits That Divide a Number

---
+++ 07LeetcodeProblems/dist/013.js
@@ -0,0 +1,17 @@
+"use strict";
+function countDigits(num) {
+    //number of digits present inside which can divide this piece of shit
+    let ans = 0;
+    let number = num;
+    //Some math function is used for that tho!!
+    //let's figure something else out
+    String(num)
+        .split("")
+        .forEach((str) => {
+        let temp = Number(str);
+        if (num % temp === 0)
+            ans++;
+    });
+    return ans;
+}
+console.log(countDigits(1248));
