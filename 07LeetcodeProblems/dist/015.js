49bb059530812aafe6b3c04f7bb7066ebe353f9b
Mayank Singh <mayank190801@gmail.com>
2023-01-30 18:09:45 +0530

    1859. Sorting the Sentence

--- 07LeetcodeProblems/dist/015.js
+++ 07LeetcodeProblems/dist/015.js
@@ -1,45 +1,48 @@
 "use strict";
-//this is basic interface simple -- no worries
 const temp = {
-    type: 0,
-    color: 1,
-    name: 2,
+    12: 0,
 };
-function countMatches1(items, ruleKey, ruleValue) {
-    let ans = 0;
-    for (let i = 0; i < items.length; i++) {
-        if (items[i][temp[ruleKey]] === ruleValue)
-            ans++;
-    }
-    return ans;
-}
-const SEARCH_CRITERIA = {
-    type: 0,
-    color: 1,
-    name: 2,
-};
-function countMatches2(items, ruleKey, ruleValue) {
-    const criteriaIndex = SEARCH_CRITERIA[ruleKey];
-    const foundItems = items.filter((item) => item[criteriaIndex] === ruleValue);
-    return foundItems === null || foundItems === void 0 ? void 0 : foundItems.length;
-}
-//one way for sure of doing this stuff
-function countMatches(items, ruleKey, ruleValue) {
-    //so if type is there then check with first and so on
-    let ans = 0;
-    let index = 0;
-    if (ruleKey === "type") {
-        index = 0;
-    }
-    else if (ruleKey === "color") {
-        index = 1;
-    }
-    else {
-        index = 2;
-    }
-    for (let i = 0; i < items.length; i++) {
-        if (items[i][index] === ruleValue)
-            ans++;
-    }
-    return ans;
-}
+console.log(temp[12]);
+// function countMatches1(
+//   items: string[][],
+//   ruleKey: string,
+//   ruleValue: string
+// ): number {
+//   let ans = 0;
+//   for (let i = 0; i < items.length; i++) {
+//   }
+//   return ans;
+// }
+// const SEARCH_CRITERIA = {
+//   type: 0,
+//   color: 1,
+//   name: 2,
+// };
+// function countMatches2(
+//   items: string[][],
+//   ruleKey: string,
+//   ruleValue: string
+// ): number {
+//   const criteriaIndex = SEARCH_CRITERIA[ruleKey];
+//   const foundItems = items.filter((item) => item[criteriaIndex] === ruleValue);
+//   return foundItems?.length;
+// }
+// //one way for sure of doing this stuff
+// function countMatches(
+//   items: string[][],
+//   ruleKey: string,
+//   ruleValue: string
+// ): number {
+//   //so
+//   let ans = 0;
+//   let index = 0;
+//     index = 0;
+//   } else
+//     index = 1;
+//   } else {
+//     index = 2;
+//   }
+//   for (let i = 0; i < items.length; i++) {
+//   }
+//   return ans;
+// }
---
+++ 07LeetcodeProblems/dist/016.js
@@ -0,0 +1,13 @@
+"use strict";
+function sortSentence(s) {
+    const str = s.split(" ");
+    str.sort((a, b) => {
+        return Number(a[a.length - 1]) - Number(b[b.length - 1]);
+    });
+    let ans = "";
+    for (let i = 0; i < str.length; i++) {
+        ans = ans + str[i].slice(0, str[i].length - 1) + " ";
+    }
+    return ans.slice(0, ans.length - 1);
+}
+console.log(sortSentence("is2 sentence4 This1 a3"));
