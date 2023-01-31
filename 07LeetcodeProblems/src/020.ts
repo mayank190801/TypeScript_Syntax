6642a169b7dabd485cdf180553fc085108a1685d
Mayank Singh <mayank190801@gmail.com>
2023-01-31 23:02:13 +0530

    2500. Delete Greatest Value in Each Row

---
+++ 07LeetcodeProblems/src/020.ts
@@ -0,0 +1,30 @@
+function deleteGreatestValue(grid: number[][]): number {
+  //so basically sort them somehow now?
+  //how to sort 2D array in this shit
+  let n = grid.length;
+  let m = grid[0].length;
+
+  for (let i = 0; i < n; i++) {
+    grid[i].sort((a, b) => {
+      return a - b;
+    });
+  }
+
+  let ans = 0;
+  for (let j = 0; j < m; j++) {
+    let cmax = 1;
+    for (let i = 0; i < n; i++) {
+      cmax = Math.max(cmax, grid[i][j]);
+    }
+    ans += cmax;
+  }
+
+  return ans;
+}
+
+console.log(
+  deleteGreatestValue([
+    [1, 2, 4],
+    [3, 3, 1],
+  ])
+);
