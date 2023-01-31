548a9b5f15261c607fec02007e889ee167a7d2af
Mayank Singh <mayank190801@gmail.com>
2023-01-31 23:08:11 +0530

    2315. Count Asterisks

---
+++ 07LeetcodeProblems/dist/021.js
@@ -0,0 +1,14 @@
+"use strict";
+function countAsterisks(s) {
+    //let's figure this out now?? no-- let's get on with this stuff bruh ---
+    const arr = s.split("|");
+    let ans = 0;
+    for (let i = 0; i < arr.length; i += 2) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] === "*")
+                ans++;
+        }
+    }
+    return ans;
+}
+console.log(countAsterisks("yo|uar|e**|b|e***au|tifu|l"));
