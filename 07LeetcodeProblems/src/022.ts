79f0020797c4545951ff761ab225ecd452735856
Mayank Singh <mayank190801@gmail.com>
2023-01-31 23:19:34 +0530

    1791. Find Center of Star Graph

---
+++ 07LeetcodeProblems/src/022.ts
@@ -0,0 +1,14 @@
+function findCenter(edges: number[][]): number {
+  //so any number repeat twice will be the answer
+  const items = new Set();
+  for (let i = 0; i < edges.length; i++) {
+    //coding crazily - open source contributer --
+    if (items.has(edges[i][0])) return edges[i][0];
+    if (items.has(edges[i][1])) return edges[i][1];
+
+    items.add(edges[i][0]);
+    items.add(edges[i][1]);
+  }
+
+  return -1;
+}
